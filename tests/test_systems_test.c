87177a9af9971bbd19a952a5a50035801149c140
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2026-05-21 21:56:12 -0700

    feat: add bit width unit tests

--- tests/test_systems_test.c
+++ tests/test_systems_test.c
@@ -30,9 +30,19 @@ void test_is_binary_str(void) {
     TEST_ASSERT_FALSE(is_binary_str(NULL));
 }
 
+void test_get_bit_width(void) {
+    TEST_ASSERT_EQUAL_size_t(0, get_bit_width(""));
+    TEST_ASSERT_EQUAL_size_t(1, get_bit_width("1"));
+    TEST_ASSERT_EQUAL_size_t(4, get_bit_width("0110"));
+    TEST_ASSERT_EQUAL_size_t(0, get_bit_width("a1"));
+    TEST_ASSERT_EQUAL_size_t(0, get_bit_width("102"));
+    TEST_ASSERT_EQUAL_size_t(0, get_bit_width(NULL));
+}
+
 int main() {
     UNITY_BEGIN();
     RUN_TEST(test_is_binary_str);
+    RUN_TEST(test_get_bit_width);
     
     return UNITY_END();
 }
