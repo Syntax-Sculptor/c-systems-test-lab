6d878f9139d20e78ffed9350869500fb80a8d24e
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2026-05-21 21:34:12 -0700

    feat: add Unity tests for binary string validation

--- tests/test_systems_test.c
+++ tests/test_systems_test.c
@@ -1,5 +1,5 @@
 /*
- * SyntaxSculptor
+ * Syntax-Sculptor
  *
  * SPDX-License-Identifier: MIT
  */
@@ -9,17 +9,30 @@
 
 #include "unity.h"
 
+#include "systems_test_lab.h"
+
 void setUp(void) {
     
 }
 
 void tearDown(void) {
-    
+
 }
 
+void test_is_binary_str(void) {
+    TEST_ASSERT_TRUE(is_binary_str("1"));
+    TEST_ASSERT_TRUE(is_binary_str("0"));
+    TEST_ASSERT_TRUE(is_binary_str("10110"));
+
+    TEST_ASSERT_FALSE(is_binary_str(""));
+    TEST_ASSERT_FALSE(is_binary_str("102"));
+    TEST_ASSERT_FALSE(is_binary_str("1a0"));
+    TEST_ASSERT_FALSE(is_binary_str(NULL));
+}
 
 int main() {
     UNITY_BEGIN();
+    RUN_TEST(test_is_binary_str);
     
     return UNITY_END();
 }
