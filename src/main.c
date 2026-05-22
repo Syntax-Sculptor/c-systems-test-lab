46ed7e0ed33d828742b0eccf494db7bae3644a6a
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2026-05-21 19:30:51 -0700

    feat: CLI input

--- src/main.c
+++ src/main.c
@@ -6,8 +6,37 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
-int main() {
-    printf("Hello world!\n");
+int is_binary_str(const char* bin_str) {
+    size_t len = strlen(bin_str);
+
+    if (len == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < len; i++) {
+        if (bin_str[i] != '1' && bin_str[i] != '0') {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: ./systems_test_lab <binary string>\n");
+        return EXIT_FAILURE;
+    }
+
+    char* bin_str = argv[1];
+
+    if (!is_binary_str(bin_str)) {
+        printf("Please enter a valid binary string.\n");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
